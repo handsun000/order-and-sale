2e326dc2591625205ba816ff4011e33d125f904d
son jin yeong <handsun000@gmail.com>
2021-10-25 15:52:00 +0900

    BeanDefinition

--- core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -13,6 +13,7 @@ public class BeanDefinitionTest {
     @Test
     @DisplayName("빈 설정 메타정보 확인")
     void findApplicationBean() {
+
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
