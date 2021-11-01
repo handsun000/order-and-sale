90cfc0612e99a0e6e1af80f069ff33c91a222c4d
son jin yeong <handsun000@gmail.com>
2021-11-01 15:48:59 +0900

    @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,18 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient //implements InitializingBean, DisposableBean
+ {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -28,4 +33,25 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
+
+     @PostConstruct
+     public void init() throws Exception {
+         connect();
+         call("초기화 연결 메시지");
+     }
+
+     @PreDestroy
+     public void close() throws Exception {
+         disconnect();
+     }
 }
